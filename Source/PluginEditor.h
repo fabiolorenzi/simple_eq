bdb0ed5ae0e8c7968a235a09376a64650bd655ac
fabiolorenzi <fabiolorenzi13975@gmail.com>
2024-03-15 14:13:45 +0000

    developed custom bypass buttons

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -6,6 +6,7 @@
 struct LookAndFeel : juce::LookAndFeel_V4
 {
     void drawRotarySlider(juce::Graphics&, int x, int y, int width, int height, float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle, juce::Slider&) override;
+    void drawToggleButton(juce::Graphics& g, juce::ToggleButton& toggleButton, bool shouldDrawButtonAsHighlighted, bool shoudDrawButtonAsDown) override;
 };
 
 struct RotarySliderWithLabels : juce::Slider
@@ -95,6 +96,8 @@ private:
     ResponseCurveComponent responseCurveComponent;
 
     std::vector<juce::Component*> getComps();
+
+    LookAndFeel lnf;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
