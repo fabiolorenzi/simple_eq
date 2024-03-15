5e28cf24a62c225f9be9e5cc30944c552fad5c86
fabiolorenzi <fabiolorenzi13975@gmail.com>
2024-03-15 12:11:46 +0000

    added decibels marks on the left of the grid for the spectrum analyzer

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -353,6 +353,15 @@ void ResponseCurveComponent::resized()
 
         g.setColour(gDb == 0.f ? Colour(255u, 0u, 0u) : Colours::lightgrey);
         g.drawFittedText(str, r, juce::Justification::centred, 1);
+
+        str.clear();
+        str << (gDb - 24.f);
+
+        r.setX(1);
+        textWidth = g.getCurrentFont().getStringWidth(str);
+        r.setSize(textWidth, fontHeight);
+        g.setColour(Colours::lightgrey);
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
     }
 }
 
